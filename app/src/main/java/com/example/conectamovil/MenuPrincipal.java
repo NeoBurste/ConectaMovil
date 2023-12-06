c7bea98b5b36c33d2e4d1527a58fa2abb8e87523
NeoBurste <raul.aguirre.rojas2000@gmail.com>
2023-12-06 10:47:56 -0300

    arreglar botones menu principal

--- app/src/main/java/com/example/conectamovil/MenuPrincipal.java
+++ app/src/main/java/com/example/conectamovil/MenuPrincipal.java
@@ -3,16 +3,19 @@ package com.example.conectamovil;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MenuPrincipal extends AppCompatActivity {
-    Button CerrarSesion, Contactos, Perfil;
+    ImageView CerrarSesion, Contactos, Perfil;
+    //Button CerrarSesion, Contactos, Perfil;
     FirebaseAuth firebaseAuth;
     FirebaseUser user;
 
